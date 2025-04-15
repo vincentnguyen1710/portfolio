ebd569f5ebbd257ea0a40068ff4bebf7da75738a
a38986640 <a38986640@gmail.com>
2025-04-15 11:51:36 -0400

    Update UI

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $(window).on("mousemove", function (event) {
     // Calculate the movement offset relative to the center of the window
-    const moveX = (event.pageX - $(window).width() / 2) * 0.02;
-    const moveY = (event.pageY - $(window).height() / 2) * 0.02;
+    const moveX = (event.pageX - $(window).width() / 2) * 0.04;
+    const moveY = (event.pageY - $(window).height() / 2) * 0.04;
     // Apply margins to the image
     $("#two").css({
       marginLeft: `${moveX}px`,
